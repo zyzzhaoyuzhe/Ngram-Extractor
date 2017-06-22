bb02bbd3575d4ce8474b222c2bbbb97929b19230
Vincent Zhao <yuzhe.zhao@yale.edu>
2017-06-21 23:35:42 -0700

    A working version

---
+++ lib_ngram.py
@@ -0,0 +1,386 @@
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+import numpy as np
+import scipy.sparse as sps
+from collections import defaultdict, deque
+import cPickle as pickle
+from nltk.tokenize import word_tokenize
+import operator
+from itertools import chain, tee
+import math
+from sys import getsizeof
+from guppy import hpy
+logger = logging.getLogger()
+logger.setLevel(20)
+
+class smartfile(object):
+    def __init__(self, filename):
+        self.fin = open(filename, 'r')
+        self.unicode = unicode
+    def __iter__(self):
+        self.fin.seek(0)
+        for line in self.fin:
+            yield line.decode('utf-8').split()
+
+class Trie(object):
+    def __init__(self):
+        self.root = {}
+
+    def add(self, word):
+        if not isinstance(word, basestring):
+            return
+        word = word.split()
+        node = self.root
+        for l in word:
+            if l not in node:
+                node[l] = {}
+            node = node[l]
+
+    def __contains__(self, word):
+        node = self.root
+        word = word.split()
+        for l in word:
+            if l in node:
+                node = node[l]
+            else:
+                return False
+        return True
+
+class BOW(object):
+    def __init__(self, ngram):
+        self.cache_uni = None
+        self.cache_ngram = None
+        self.map = None
+        self.ngram = ngram
+
+    def raw_count(self, file):
+        """
+        batch
+        """
+        logger.info('Extracting features: raw_count')
+        ## get dimensions
+        nfeatures = len(self.map)
+        nrows = 0
+        fs = open(file, 'r')
+        for _ in fs:
+            nrows += 1
+        ## get features
+        fs = open(file, 'r')
+        label = np.empty(nrows, dtype=np.int)
+        cache, row_idx, col_idx = [], [0], []
+        for idx, line in enumerate(fs):
+            if idx%10000 == 0:
+                logger.debug('Finish {} samples, {}%'.format(idx, int(float(idx)/nrows*100)))
+            line = line.decode('utf-8').strip().split(',')
+            l = int(line[0])
+            line = line[1:]
+            label[idx] = l
+            foo = defaultdict(int)
+            for sent in line:
+                sent = sent.split()
+                ### add all unigram
+                for w in sent:
+                    if w in self.map:
+                        foo[self.map[w]] += 1
+                ### add ngram frome long to short
+                # version 1 (include substr)
+                for i in range(len(sent)-1):
+                    for j in range(i+1, min(len(sent), i + self.ngram)):
+                        w = ' '.join(sent[i:j + 1])
+                        if w in self.map:
+                            foo[self.map[w]] += 1
+                # # version 2 (don't include substr)
+                # i = 0
+                # while i < len(sent) - 1:
+                #     for j in range(min(len(sent), i + self.ngram) - 1, i, -1):
+                #         w = ' '.join(sent[i:j + 1])
+                #         if w in self.map:
+                #             foo[self.map[w]] += 1
+                #             i = j
+                #     i += 1
+            col_idx += foo.keys()
+            cache += foo.values()
+            row_idx.append(len(cache))
+        data = sps.csr_matrix((cache, col_idx, row_idx), shape=(nrows, nfeatures), dtype=np.int)
+        return data, label
+
+    @staticmethod
+    def _prune(cache, thre):
+        tbd = []
+        for key in cache.iterkeys():
+            if cache[key] < thre:
+                tbd.append(key)
+        for key in tbd:
+            del cache[key]
+        del tbd
+
+    def prune(self, cache, thre, maxmem):
+        firsttimmer = 1
+        while len(cache) > 0.5 * maxmem:
+            if not firsttimmer:
+                thre += 1
+            self._prune(cache, thre)
+            firsttimmer = 0
+        return thre
+
+    def count(self, file, maxmem=(1000000, 10000000), dic=None, min_count=10, per=3000):
+        """
+        Count the frequency of unigram and ngram (up to self.ngram)
+        :param file:
+        :param maxmem: maximal memory usage
+        :param dic: contain key-value pairs as (ngram, wpmi)
+        :param min_count:
+        :param per:
+        :return:
+        """
+        fp = open(file, 'r')
+        cache_ngram = defaultdict(int)
+        cache_uni = defaultdict(int)
+        total = [0] * self.ngram
+        thre = [2] * 2
+        # track memory usage
+        h = hpy()
+        for idx, line in enumerate(fp):
+            line = line.decode('utf-8').strip().split(',')[1:]  # for Zhang's datasets
+            # line = [line.decode('utf-8')]   # for wiki dumps
+            if idx % per == 0:
+                logger.debug('Finish {} lines with {} unigrams@{} and {} ngrams@{}'.format(idx, len(cache_uni), thre[0], len(cache_ngram), thre[1]))
+                # print h.heap()
+            for sent in line:
+                sent = sent.split()
+                len_sent = len(sent)
+                # calculate total
+                for i in range(self.ngram):
+                    total[i] += len_sent - i
+                # count a uni-gram
+                for w in sent:
+                    cache_uni[w] += 1
+                # count n-grams
+                if not dic:
+                    for i in xrange(len_sent - 1):
+                        for j in xrange(i+1, min(len_sent, i + self.ngram)):
+                            w = ' '.join(sent[i:j+1])
+                            cache_ngram[w] += 1
+                else:
+                    dp = [None] * (2 * len_sent - 1)
+                    for ng in xrange(self.ngram, 1, -1):
+                        for i in xrange(len_sent - ng +1):
+                            w = ' '.join(sent[i:i+ng])
+                            pos = ng - 1 + 2 * i
+                            if w in dic:
+                                dp[pos] = max((dic[w], dp[pos-1], dp[pos+1]))
+                                if dic[w] == dp[pos]:
+                                    cache_ngram[w] += 1
+                            else:
+                                dp[pos] = max((dp[pos-1], dp[pos+1]))
+                # # old count n-grams
+                # i = 0
+                # while i < len(sent):
+                #     for j in range(min(len(sent), i + self.ngram) - 1, i, -1):
+                #         w = ' '.join(sent[i:j+1])
+                #         cache_ngram[w] += 1
+                #         total[j - i] += 1
+                #         if w in dic:
+                #             i = j
+                #             break
+                #     i += 1
+                # prune
+                if len(cache_uni) > maxmem[0]:
+                    thre[0] = self.prune(cache_uni, thre[0], maxmem[0])
+                if len(cache_ngram) > maxmem[1]:
+                    thre[1] = self.prune(cache_ngram, thre[1], maxmem[1])
+        # final prune:
+        self._prune(cache_uni, max(min_count, thre[0]))
+        self._prune(cache_ngram, max(min_count, thre[1]))
+        return cache_uni, cache_ngram, total
+
+    @staticmethod
+    def bestN(cache_uni, cache_ngram, topN, mode='s'):
+        if mode == 's':
+            if isinstance(topN, int):
+                cache_uni = sorted(cache_uni.items(), key=operator.itemgetter(1), reverse=True)[:topN]
+                cache_ngram = sorted(cache_ngram.items(), key=operator.itemgetter(1), reverse=True)[:topN]
+                return cache_uni, cache_ngram
+            else:
+                cache_uni = sorted(cache_uni.items(), key=operator.itemgetter(1), reverse=True)[:topN[0]]
+                cache_ngram = sorted(cache_ngram.items(), key=operator.itemgetter(1), reverse=True)[:topN[1]]
+                return cache_uni, cache_ngram
+        elif mode == 'j':
+            return sorted(cache_uni.items() + cache_ngram.items(), key=operator.itemgetter(1), reverse=True)[:topN]
+
+    def get_map(self, cache_uni, cache_ngram):
+        """
+        Get ngram to index map
+        :param cache_uni:
+        :param cache_ngram:
+        :return:
+        """
+        self.map = {}
+        for idx, w in enumerate(chain(cache_uni, cache_ngram)):
+            w = w[0]
+            self.map[w] = idx
+
+    def save(self, filename):
+        to_save = ['cache_uni', 'cache_ngram', 'map', 'ngram', 'cache_uni_wpmi', 'total']
+        dic = self.__dict__
+        tbs = {}
+        for key in to_save:
+            if key in dic:
+                tbs[key] = dic[key]
+        pickle.dump(tbs, open(filename, 'w'))
+
+    def load(self, filename):
+        tbd = pickle.load(open(filename, 'r'))
+        for key, val in tbd.iteritems():
+            self.__dict__[key] = val
+
+class BOW_freq(BOW):
+    def get_ngram(self, file, topN, maxmem=(1000000, 10000000), mode='s'):
+        logger.info('BOW_freq: get_ngrams')
+        cache_uni, cache_ngram, _ = self.count(file, maxmem=maxmem, min_count=10)
+        #
+        if mode == 's':
+            cache_uni, cache_ngram = self.bestN(cache_uni, cache_ngram, topN, mode='s')
+            foo = chain(cache_uni, cache_ngram)
+        elif mode == 'j':
+            foo = self.bestN(cache_uni, cache_ngram, topN, mode='j')
+        self.get_map(foo, [])
+        self.cache_uni = cache_uni
+        self.cache_ngram = cache_ngram
+        if mode == 's':
+            logger.info('BOW_freq is finished; {} unigram features and {} ngram features'.format(len(cache_uni), len(cache_ngram)))
+        elif mode == 'j':
+            logger.info('BOW_freq is finished; {} unigram features and ngram features totally.'.format(len(self.map)))
+
+class BOW_wpmi(BOW):
+    # @staticmethod
+    # def get_dic(cache):
+    #     logger.info('BOW_wpmi: Generate Dictionary from current ngrams')
+    #     # dic = set()
+    #     # substr = Trie()
+    #     # for w, _ in cache:
+    #     #     if w in substr:
+    #     #         continue
+    #     #     dic.add(w)
+    #     #     w = w.split()
+    #     #     for j in range(len(w) - 1):
+    #     #         substr.add(' '.join(w[j:]))
+    #     # del substr
+    #     # logger.info('BOW_wpmi: Dictionary size {}'.format(len(dic)))
+    #     dic = {}
+    #     for key, val in cache:
+    #         dic[key] = val
+    #     return dic
+
+    @staticmethod
+    def _get_ngram(cache_uni, cache_ngram, total):
+        """ Calculate wpmi"""
+        logtotal = [math.log(val) if val > 0 else None for val in total]
+        log_min_val = math.log(min(cache_uni.itervalues()))
+        for key, val in cache_ngram.iteritems():
+            words = key.split()
+            l = len(words)
+            logwords = [math.log(cache_uni[w]) if w in cache_uni else log_min_val for w in words]
+            cache_ngram[key] = val * (math.log(val) -
+                                      logtotal[l - 1] -
+                                      sum(logwords) +
+                                      l * logtotal[0])
+
+    def get_ngram(self, file, topN, maxmem=(1000000, 10000000), dic=set()):
+        logger.info('BOW_wpmi: get_ngrams')
+        ## stage 1
+        cache_uni, cache_ngram, total = self.count(file, maxmem=maxmem, min_count=10)
+        # calculate wpmi
+        self._get_ngram(cache_uni,cache_ngram, total)
+        # stage 2
+        cache_uni, cache_ngram, total = self.count(file, maxmem=maxmem, min_count=10, dic=cache_ngram)
+        self._get_ngram(cache_uni, cache_ngram, total)
+        cache_uni, cache_ngram = self.bestN(cache_uni, cache_ngram, topN)
+        # ## old
+        # for i in range(niter):
+        #     # get counts
+        #     cache_uni, cache_ngram, total = self.count(file, maxmem=maxmem, dic=dic, min_count=10)
+        #     logtotal = [math.log(val) if val > 0 else None for val in total]
+        #     # caclulate wpmi and store in cache_ngram
+        #     min_val = None
+        #     for val in cache_uni.itervalues():
+        #         if not min_val or min_val > val:
+        #             min_val = val
+        #     for key, val in cache_ngram.iteritems():
+        #         words = key.split()
+        #         l = len(words)
+        #         logwords = [math.log(cache_uni[w]) if w in cache_uni else math.log(min_val) for w in words]
+        #         cache_ngram[key] = val * (math.log(val) -
+        #                                  logtotal[l - 1] -
+        #                                  sum(logwords) +
+        #                                  l * logtotal[0])
+        #     # take best N
+        #     cache_uni, cache_ngram = self.bestN(cache_uni, cache_ngram, topN)
+        #     if i == niter - 1:
+        #         break
+        #     # get dic
+        #     dic = self.get_dic(cache_ngram)
+        # get ngram to index mapping
+        self.get_map(cache_uni, cache_ngram)
+        self.cache_uni, self.cache_ngram = cache_uni, cache_ngram
+        logger.info('BOW_wpmi is finished; {} unigram features and {} ngram features are learned'.format(len(cache_uni), len(cache_ngram)))
+
+    def rank_unigram_wpmi(self, file, maxmem=(1000000, 10000000)):
+        # get counts
+        self.ngram = 2
+        cache_uni, cache_bi, total = self.count(file, maxmem=maxmem, min_count=10)
+        # logtotal = [math.log(val) if val > 0 else None for val in total]
+        # # get PMI for cache_bi
+        # for key, val in cache_bi.iteritems():
+        #     words = key.split()
+        #     if all([w in cache_uni for w in words]):
+        #         # weighted pmi
+        #         # cache_bi[key] = val * (math.log(val) -
+        #         #               logtotal[1] -
+        #         #               math.log(cache_uni[words[0]]) -
+        #         #               math.log(cache_uni[words[1]]) +
+        #         #               2 * logtotal[0])
+        #         # PMI
+        #         v = (math.log(val) -
+        #              logtotal[1] -
+        #              math.log(cache_uni[words[0]]) -
+        #              math.log(cache_uni[words[1]]) +
+        #              2 * logtotal[0])
+        #         cache_bi[key] = v
+        # # get new rank
+        # cache_uni_wpmi = defaultdict(float)
+        # for key, val in cache_bi.iteritems():
+        #     words = key.split()
+        #     if all([w in cache_uni for w in words]):
+        #         cache_uni_wpmi[words[0]] += val
+        #         cache_uni_wpmi[words[1]] += val
+        self.cache_uni, self.cache_ngram = cache_uni, cache_bi
+        self.total = total
+
+
+if __name__ == '__main__':
+    text = smartfile('/media/vincent/Data-adhoc/wiki_dumps/wiki_zh/zhwiki-article')
+    obj = BOW_wpmi(5)
+    topN = 1000000
+    obj.get_ngram(text, topN, niter=1)
+
+    with open('zhwiki-article-{}gram-freq-list'.format(5), 'w') as h:
+        for idx, item in enumerate(obj.cache_ngram):
+            h.write('{}\t{}\t{}\n'.format(idx, item[0].encode('utf-8'), item[1]))
+
+    dic = obj.get_dic(obj.cache_wpmi)
+    obj.get_ngram(text, topN, niter=1, dic=dic)
+
+    with open('zhwiki-article-{}gram-wpmi-list-iter{}'.format(5, 2), 'w') as h:
+        for idx, item in enumerate(obj.cache_wpmi):
+            h.write('{}\t{}\t{}\n'.format(idx,
+                                          item[0].encode('utf-8'),
+                                          math.log(item[1]) if item[1] > 0 else 0))
+
+
+
+
+
+
+
--- test_classification.py
+++ test_classification.py
@@ -1,24 +1,134 @@
+"""
+Feed training data to classifier
+"""
+
 import logging
+from time import time
 import numpy as np
+from scipy.sparse import csr_matrix
 from collections import defaultdict
-from nltk.tokenize import word_tokenize
-import lib_ngrams
-import math
+from Queue import Queue
+import matplotlib.pyplot as plt
+import threading, multiprocessing
+from time import sleep
+from nltk.tokenize import word_tokenize, RegexpTokenizer, sent_tokenize
+from nltk.stem import WordNetLemmatizer
+import lib_ngram
+import math, operator
+import pickle
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
 
 
-class feature(object):
-    def __init__(self, file):
-        self.label = []
-        self.data = []
+logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(levelname)s : %(message)s')
+TOKER = RegexpTokenizer(r'\w+')
+WNL = WordNetLemmatizer()
+TFIDF = TfidfTransformer(norm='l2', sublinear_tf=False, use_idf=True)
 
-class BOW_count(feature):
-    def load(self):
+def benchmark(clf, X_train, y_train, X_test, y_test):
+    print('_' * 80)
+    print("Training: ")
+    print(clf)
+    t0 = time()
+    clf.fit(X_train, y_train)
+    train_time = time() - t0
+    print("train time: %0.3fs" % train_time)
+    #
+    t0 = time()
+    pred = clf.predict(X_train)
+    score_time = time() - t0
+    print("score time:  %0.3fs" % score_time)
+    train_accu = metrics.accuracy_score(y_train, pred)
+    print('Train accuracy:   %0.3f; Error rate  %0.3f' % (train_accu, 1-train_accu))
+    #
+    t0 = time()
+    pred = clf.predict(X_test)
+    test_time = time() - t0
+    print("test time:  %0.3fs" % test_time)
+    test_accu = metrics.accuracy_score(y_test, pred)
+    print('Test accuracy:   %0.3f; Error rate   %0.3f' % (test_accu, 1-test_accu))
+    clf_descr = str(clf).split('(')[0]
+    return clf_descr, train_accu, test_accu, train_time
 
-def load_zhang(file):
-    h = open(file, 'r')
-    for line in h:
-        line = line.split('","',2)
-        label = int(line[0][1:])
-        title = line[1]
-        body = line[2][:-3]
-        yield label, title, body
+# if __name__ == '__main__':
+print ' '
+print ' '
+
+job_queue = ['ag_news_csv', 'dbpedia_csv', 'yahoo_answers_csv',
+             'amazon_review_full_csv', 'amazon_review_polarity_csv',
+             'yelp_review_polarity_csv', 'yelp_review_full_csv']
+
+# Try classifier
+def worker(dataset):
+    logging.info('Dataset: {}'.format(dataset))
+    logfile = 'result_classify.log'
+    h = open(logfile, 'a')
+    # folder = '../DATA/TextClassificationDatasets/'
+    try:
+        ## freq
+        m, X_train, y_train, X_test, y_test = pickle.load(open(dataset + '_freq_traindata.p','r'))
+        # count to tfidf
+        X_train = TFIDF.fit_transform(X_train)
+        X_test = TFIDF.fit_transform(X_test)
+        # clf_descr, train_accu, test_accu, train_time = benchmark(LinearSVC(loss='hinge', penalty='l2'), X_train,
+        #                                                          y_train, X_test, y_test)
+        clf_descr, train_accu, test_accu, train_time = benchmark(LogisticRegression(penalty='l2', n_jobs=4), X_train,
+                                                                 y_train, X_test, y_test)
+        #
+        logging.info('{}\t{}\ttrain_accu:{}\ttest_accu:{}'.format(dataset+'_freq', clf_descr, train_accu, test_accu))
+        #
+        h.write('{}\t{}\ttrain_accu:{}\ttest_accu:{}\t#features:{}\n'.format(dataset + '_freq', clf_descr, train_accu,
+                                                                             test_accu, len(m)))
+        del m, X_train, y_train, X_test, y_test
+        ## PMI
+        # m, X_train, y_train, X_test, y_test = pickle.load(open(dataset + '_wpmi_traindata.p', 'r'))
+        # # count to tfidf
+        # X_train = TFIDF.fit_transform(X_train)
+        # X_test = TFIDF.fit_transform(X_test)
+        # clf_descr, train_accu, test_accu, train_time = benchmark(LinearSVC(loss='hinge', penalty='l2'), X_train,
+        #                                                          y_train, X_test, y_test)
+        # h.write('{}\t{}\ttrain_accu:{}\ttest_accu:{}\t#features:{}\n'.format(dataset + '_wpmi', clf_descr, train_accu,
+        #                                                                      test_accu, len(m)))
+        # clf_descr, train_accu, test_accu, train_time = benchmark(LogisticRegression(penalty='l2', n_jobs=4), X_train,
+        #                                                          y_train, X_test, y_test)
+        # h.write('{}\t{}\ttrain_accu:{}\ttest_accu:{}\t#features:{}\n'.format(dataset + '_wpmi', clf_descr, train_accu,
+        #                                                                      test_accu, len(m)))
+        # del m, X_train, y_train, X_test, y_test
+        # return dataset, clf_descr, train_accu, test_accu, train_time
+    except:
+        logging.info('{}----failed----'.format(dataset))
+    h.close()
+
+
+# worker(job_queue[0])
+
+multiprocessing.Pool(8).map(worker, job_queue)
+
+# dataset = job_queue[3]
+# worker(dataset)
+
+    # jobs = []
+    # nproc = 4
+    # result = multiprocessing.Pool(nproc).map(worker_loop, zip(job_queue, [topN]*len(job_queue)))
+    # pickle.dump(result, open('result.p','w'))
+    #
+    # foo = []
+    # for item in result:
+    #     if item:
+    #         foo += item
+    # result = foo
+    # # display results
+    # indices = np.arange(len(result))
+    # descrip, train_accu, test_accu = [[x[i] for x in result] for i in [0,3,4]]
+    # plt.figure()
+    # plt.barh(indices, train_accu, .3, label='train_accu', color='navy')
+    # plt.barh(indices + 0.4, test_accu, .3, label='test_accu', color='c')
+    # plt.yticks(())
+    # for i, c in zip(indices, descrip):
+    #     plt.text(-.1, i, c)
+    #
+    # print 'done'
